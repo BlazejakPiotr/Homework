38ea9b47539fab07de5076ec71e944c80312ce97
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-06-25 14:45:39 +0200

    Finished "EXERCISE 5"

--- M1D5-Homework.js
+++ M1D5-Homework.js
@@ -42,7 +42,14 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+    function strivify(str){
+        return (str === "Strive") ? str : `Strive ${str}`
+    }
+    // Loging string "Strive"
+    // console.log(strivify("Strive"))
+
+    // Logging string "Piotr"
+    // console.log(strivify("Piotr"))
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
